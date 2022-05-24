704af59bf3415f0de38afdc58a06bfe3207578c9
Fernando <motercode@gmail.com>
2022-05-24 10:18:29 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'vittesse app for use',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'configuring with pinia normal sintax and chakra components' },
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
